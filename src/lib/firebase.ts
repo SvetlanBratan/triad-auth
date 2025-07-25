a4e744d33345e452383f62a2bac489424a082b90
Светлана Каверзина <kaverzinasweetlana@gmail.com>
2025-07-25 21:29:36 +0000

    браузер не запоминает, что я входила уже? ссесии сбрасываются?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, initializeAuth, indexedDBLocalPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
@@ -19,8 +19,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// Initialize Auth with persistence
+export const auth = initializeAuth(app, {
+  persistence: indexedDBLocalPersistence
+});
+
 
 // Initialize Analytics only in the browser
 if (typeof window !== 'undefined') {
