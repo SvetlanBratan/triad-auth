cbc392b83c0f49c59a496df7010424b78ac0c28d
Светлана Каверзина <kaverzinasweetlana@gmail.com>
2025-07-26 18:03:51 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
@@ -32,4 +31,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
