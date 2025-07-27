659e36a55d501a8ac8052bb2473164e191a8ccff
Светлана Каверзина <kaverzinasweetlana@gmail.com>
2025-07-27 12:22:29 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
+  experimental: {
+    // This is a workaround for a file watching issue in some environments.
+    instrumentationHook: false,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
+  watchOptions: {
+    // Ignoring the config file should prevent the restart loop.
+    ignored: ['**/.next/**', '**/node_modules/**', '**/next.config.mjs'],
+  },
+  devIndicators: {
+    allowedDevOrigins: ['*.cloudworkstations.dev'],
   },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
+        hostname: 'i.postimg.cc',
       },
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
       },
     ],
   },
