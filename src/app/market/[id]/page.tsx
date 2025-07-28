506b298016426013bd5d9e3b296a6971f1734493
Светлана Каверзина <kaverzinasweetlana@gmail.com>
2025-07-28 21:43:55 +0000

    SyntaxError: Missing initializer in destructuring declaration

--- src/app/market/[id]/page.tsx
+++ src/app/market/[id]/page.tsx
@@ -37,7 +37,7 @@ import { Textarea } from '@/components/ui/textarea';
 export default function ShopPage() {
     const { id } = useParams();
     const router = useRouter();
-    const { currentUser, deleteShopItem, purchaseShopItem, restockShopItem, fetchShopById, updateShopDetails } from 'useUser';
+    const { currentUser, deleteShopItem, purchaseShopItem, restockShopItem, fetchShopById, updateShopDetails } = useUser();
     const { toast } = useToast();
     
     const shopId = Array.isArray(id) ? id[0] : id;
