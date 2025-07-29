9a72f590fa83dc3ce7d4cd73f91e3a35bea68e39
Светлана Каверзина <kaverzinasweetlana@gmail.com>
2025-07-29 16:09:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.postimg.cc',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  experimental: {},
-  allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.postimg.cc',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'ik.imagekit.io',
+                port: '',
+                pathname: '/**',
+            }
+        ]
+    },
+    experimental: {
+    },
+    allowedDevOrigins: ["https://*.cloudworkstations.dev"],
 };
 
 export default nextConfig;
