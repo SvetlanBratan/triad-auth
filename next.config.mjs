012f548ecea6880d3da86227005594c487c8042a
Светлана Каверзина <kaverzinasweetlana@gmail.com>
2025-10-12 15:55:59 +0000

    18:51:20.078 Running build in Washington, D.C., USA (East) – iad1 18:51:

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: ["*.cloudworkstations.dev"],
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
+       {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
       {
         protocol: 'https',
         hostname: 'i.postimg.cc',
       },
-      {
+       {
         protocol: 'https',
-        hostname: 'placehold.co',
+        hostname: 'ik.imagekit.io',
       },
     ],
   },
