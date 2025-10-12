cc992a03e6f055e6f1ed683e5dd229d8f192f6af
Светлана Каверзина <kaverzinasweetlana@gmail.com>
2025-10-12 15:31:32 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: ["*.cloudworkstations.dev"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
