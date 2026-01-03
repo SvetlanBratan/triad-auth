e1b62f09d16972aacfffc5638858a390e02fbe09
Светлана Каверзина <kaverzinasweetlana@gmail.com>
2026-01-03 13:36:19 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+      },
+      {
+        protocol: 'https',
+        hostname: 'framerusercontent.com',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
