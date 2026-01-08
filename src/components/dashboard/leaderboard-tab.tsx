1bfaacfe49a2cf774c207ecd3342f8534b9855dd
Светлана Каверзина <kaverzinasweetlana@gmail.com>
2026-01-08 10:52:22 +0000

    я про вот это

--- src/components/dashboard/leaderboard-tab.tsx
+++ src/components/dashboard/leaderboard-tab.tsx
@@ -297,7 +297,9 @@ export default function LeaderboardTab() {
                 <TabsTrigger value="search"><Search className="mr-2 h-4 w-4"/>Поиск соигроков</TabsTrigger>
             </TabsList>
             <TabsContent value="leaderboard" className="mt-4">
-                 <p className="text-center text-muted-foreground mb-4 line-through">Список Forbes</p>
+                 <p className="text-center text-muted-foreground mb-4">
+                    Список <span className="line-through">Forbes</span> Тыквенного Переполоха. Нажмите на пользователя, чтобы просмотреть детали.
+                </p>
                 <LeaderboardTable />
             </TabsContent>
             <TabsContent value="search" className="mt-4">
